bc6302962f59df1da894a694dd1bea6147c6e892
Andrei Kosintsev <andrei.kosintsau@gmail.com>
2023-12-22 12:12:46 +0100

    selected multiply groups

--- bot/scripts/selector.js
+++ bot/scripts/selector.js
@@ -6,74 +6,107 @@ tg.expand();
 
 // Set the main button text and make it visible
 tg.MainButton.text = "Select Program";
-
+let selectedMuscleGroups = [];
 
 document.addEventListener('DOMContentLoaded', function () {
-            // Get all muscle group IDs
-            const muscleGroupIds = [
-                'Trapezius', 'Lats', 'Triceps', 'Forearms',
-                'Glutes', 'Hamstrings', 'Calves', 'Deltoids',
-                'Biceps', 'Pectorals', 'Abs',
-                'Quads', 'Adductors','Levator Scapulae','Abductors','Spine', 'Serratus Anterior', 'Upper back'
-            ];
+    // Get all muscle group IDs
+    const muscleGroupIds = [
+        'Trapezius', 'Lats', 'Triceps', 'Forearms',
+        'Glutes', 'Hamstrings', 'Calves', 'Deltoids',
+        'Biceps', 'Pectorals', 'Abs',
+        'Quads', 'Adductors','Levator Scapulae','Abductors','Spine', 'Serratus Anterior', 'Upper back'
+    ];
 
-            // Add mouseover event listener to each muscle group
-            muscleGroupIds.forEach(function (muscleId) {
-                const muscleElement = document.getElementById(muscleId);
+    // Add mouseover event listener to each muscle group
+    muscleGroupIds.forEach(function (muscleId) {
+        const muscleElement = document.getElementById(muscleId);
 
-                // Change color on mouseover
-                muscleElement.addEventListener('mouseover', function () {
-                    const pathElement = muscleElement.querySelector('path');
-                    if (pathElement.classList) {
-                      pathElement.classList.add("hover");
-                    } else {
-                      pathElement.className += ' ' + "hover";
-                    }
-                    document.getElementById("select").textContent = muscleId;
-                });
-
-                // Restore original color on mouseout
-                muscleElement.addEventListener('mouseout', function () {
-                    muscleElement.style.fill = ''; // Restore original color
-                    const pathElement = muscleElement.querySelector('path');
-                    if (pathElement.classList) {
-                        pathElement.classList.remove("hover");
-                    }
-                    document.getElementById("select").textContent = 'Choose a Muscle';
-                });
-                muscleElement.addEventListener('click', function () {
-                    const pathElement = muscleElement.querySelector('path');
-                    if (pathElement.classList) {
-                        pathElement.classList.add("hover");
-                    } else {
-                        pathElement.className += ' ' + "hover";
-                    }
-                    document.getElementById("select").textContent = muscleId;
-                    tg.MainButton.show();
-                });
-            });
+        muscleElement.addEventListener('mouseover', function () {
+            const pathElement = muscleElement.querySelector('path');
+            if (!pathElement.classList.contains("hover")) {
+                if (pathElement.classList) {
+                    pathElement.classList.add("hover");
+                } else {
+                    pathElement.className += ' ' + "hover";
+                }
+            }
         });
 
+        muscleElement.addEventListener('mouseout', function () {
+            const pathElement = muscleElement.querySelector('path');
+            if (!pathElement.classList.contains("hover")) {
+                if (pathElement.classList) {
+                    pathElement.classList.remove("hover");
+                }
+            }
+        });
 
-// Event handler for the main button click
-Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    const pageTitleText = document.getElementById("select").textContent;
-    if (pageTitleText==="Choose a Muscle") {
-            // Display a message to choose a category if none is selected
+        muscleElement.addEventListener('click', function () {
+            const pathElement = muscleElement.querySelector('path');
+            const allPath = muscleElement.querySelectorAll('path');
+
+            // Check if the muscle group is already selected
+            if (selectedMuscleGroups.includes(muscleId)) {
+                // Deselect the muscle group
+                selectedMuscleGroups = selectedMuscleGroups.filter(group => group !== muscleId);
+                allPath.forEach(function (pathElement) {
+                    pathElement.style= "fill: #333;";
+                });
+            } else {
+                // Check if the maximum limit of 5 selections is reached
+                if (selectedMuscleGroups.length < 5) {
+                    // Select the muscle group
+                    selectedMuscleGroups.push(muscleId);
+                    allPath.forEach(function (pathElement) {
+                        pathElement.style= "fill: black !important;opacity: .5; cursor: pointer;";
+                    });
+                } else {
+                    // Display a message if the maximum limit is reached
                     tg.showPopup({
-                        title: 'Choose a Category',
-                        message: 'You have not selected a muscle category for your workout.',
+                        title: 'Maximum Selection Reached',
+                        message: 'You can only select up to 5 muscle groups.',
                         buttons: [{
-                            id: 'delete',
+                            id: 'ok',
                             type: 'ok',
                             text: 'OK'
                         }]
                     });
+                }
+            }
+            tg.MainButton.show();
+        });
+    });
+});
+
+// Event handler for the main button click
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+    // Check if no muscle groups are selected
+    if (selectedMuscleGroups.length === 0) {
+        tg.showPopup({
+            title: 'Choose a Category',
+            message: 'You have not selected any muscle categories for your workout.',
+            buttons: [{
+                id: 'ok',
+                type: 'ok',
+                text: 'OK'
+            }]
+        });
+    } else if (selectedMuscleGroups.length > 4) {
+        // Display a message if more than 4 muscle groups are selected
+        tg.showPopup({
+            title: 'Maximum Selection Reached',
+            message: 'You can only select up to 5 muscle groups.',
+            buttons: [{
+                id: 'ok',
+                type: 'ok',
+                text: 'OK'
+            }]
+        });
     } else {
         // Send the selected muscle categories to the Telegram WebApp and close the app
-                    tg.sendData(JSON.stringify({
-                        muscle_group: pageTitleText
-                    }));
-                    tg.close();
+        tg.sendData(JSON.stringify({
+            muscle_group: selectedMuscleGroups
+        }));
+        tg.close();
     }
 });
