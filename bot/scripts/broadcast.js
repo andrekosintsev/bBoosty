26d7a759efeeadb5b94c7c466194a4f48d6bc7be
Andrei Kosintsev <andrei.kosintsau@gmail.com>
2024-02-19 13:13:21 +0100

    by language added

--- bot/scripts/broadcast.js
+++ bot/scripts/broadcast.js
@@ -9,9 +9,10 @@ const message = document.getElementById('message');
 
 // Event handler for the main button click
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    const selectedLanguage = languageSelect.value;
     tg.sendData(JSON.stringify({
-                lang: language,
-                text: message
+                lang: selectedLanguage,
+                text: message.value
             }));
             tg.close();
 });
