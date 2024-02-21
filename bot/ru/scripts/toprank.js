e7d2e0768de1a8e6b561e7edfb0597cd89093d35
Andrei Kosintsev <andrei.kosintsau@gmail.com>
2024-02-21 22:54:17 +0100

    toprank

--- bot/ru/scripts/toprank.js
+++ bot/ru/scripts/toprank.js
@@ -14,40 +14,36 @@ document.addEventListener("DOMContentLoaded", function () {
     if (encodedJsonData) {
         const jsonData = decodeURIComponent(encodedJsonData);
         const jsonObject = JSON.parse(jsonData);
-        populateFormForEditing(jsonObject);
+        const tops = document.getElementById('tops');
+        jsonObject.forEach(x => {
+            tops.appendChild(populateFormForEditing(x));
+        });
     }
-    const tops = document.getElementById('tops');
-    [{'id':'49e22002-0ebe-430c-acd0-e83923625de6',
-    'fullName':'Andros Papandros',
-    'count': 123,
-    'muscle': 'triceps, pectoral'},{'id':'49e22002-0ebe-430c-acd0-e83923625de6', 'count': 15,'muscle': 'shoulders, triceps, forearms, chest'}].forEach(x => {
-        tops.appendChild(populateFormForEditing(x));
-    });
 });
 
 function populateFormForEditing(entry) {
     const colDiv = document.createElement('div');
     colDiv.classList.add('col-lg-4');
-    const linkProgram = 'https://t.me/BBoostyBot?start='+entry.id;
+    const linkProgram = 'https://t.me/TrainMateBot?start='+entry.id;
     const name = !entry.fullName ? 'Имя не установлено' : entry.fullName;
     colDiv.innerHTML = `
             <div class="card card-small card-post mb-4">
                 <div class="card-body d-flex">
                     <div class="card-post__author d-flex">
-                        <a href="#" class="card-post__author-avatar card-post__author-avatar--small" style="background-image: url('images/logo.png');">Written by James Khan</a>
+                        <a href="#" class="card-post__author-avatar card-post__author-avatar--small" style="background-image: url('images/logo.png');"></a>
                         <div class="d-flex flex-column justify-content-center ml-3">
-                            <span class="card-post__author-name">Автор: ${name}</span>
-                            <small class="text-muted">Получили программу: <br>${entry.count} человек</small>
-                            <small class="text-muted"> ${entry.muscle}</small>
+                            <span class="card-post__author-name">${name}</span>
+                            <small class="text-muted">Использовали программу: <br>${entry.count} человек</small>
+                            <small class="text-muted">Мышечная группа:<br> ${entry.muscle}</small>
+                            ${entry.promoted ? `<small style="color: #B8860B;">Promoted</small>` : ''}
                         </div>
                     </div>
                     <div class="my-auto ml-auto">
                         <a class="btn btn-sm btn-white" href=${linkProgram}>
-                            <i class="far fa-bookmark mr-1"></i>Получить</a>
+                            <i class="fa fa-play mr-1"></i></a>
                     </div>
                 </div>
             </div>
         `;
         return colDiv;
-
 }
